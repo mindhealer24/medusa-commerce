3d6afbd29f89433f8d3b1e189a752ef10e2a456f
mindhealer24 <mindhealer247@gmail.com>
2025-03-20 13:38:23 +0530

    Fix Sanity module path

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -83,7 +83,7 @@ export default defineConfig({
       }
     },
     {
-      resolve: "./modules/sanity",
+      resolve: "./src/modules/sanity",
       options: {
         api_token: process.env.SANITY_API_TOKEN,
         project_id: process.env.SANITY_PROJECT_ID,
